643b78b9b18e56b5728ba80036eb450c06332dc3
XxxxYxxx <81127922+XxxxYxxx@users.noreply.github.com>
2021-04-06 14:26:02 +0800

    Update import_superzk.go
    modify cgo

--- c_superzk/import_superzk.go
+++ c_superzk/import_superzk.go
@@ -6,7 +6,7 @@ import _ "github.com/sero-cash/go-czero-import/czero"
 
 #cgo CFLAGS: -I ../czero/szk_include
 
-#cgo LDFLAGS: -L ../czero/lib -lsuperzk
+#cgo LDFLAGS: -L ../czero/lib_LINUX_AMD64_v4 -lsuperzk
 
 */
 import "C"
